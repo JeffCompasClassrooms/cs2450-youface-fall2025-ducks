9c1ca39566bf174eba668e0d393a3f8bcc62b962
Obed Vazquez <obedvazquez056@gmail.com>
2025-11-05 09:36:55 -0700

    Modified tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -9,7 +9,14 @@ options.add_argument("--headless")  # Run Chrome invisibly
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+driver = webdriver.Remote(
+    command_executor=selenium_url,
+    options=options,
+    desired_capabilities=DesiredCapabilities.CHROME
+)
 
 try:
     driver.get("http://127.0.0.1:5005")

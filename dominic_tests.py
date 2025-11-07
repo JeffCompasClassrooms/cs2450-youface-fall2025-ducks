f030dc81da287a055e1b4d85259575efd659371c
Brandon Garcia <brandon.garcia.orea@gmai.com>
2025-11-07 09:36:18 -0700

    fixes code 3

--- dominic_tests.py
+++ dominic_tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 options = Options()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 

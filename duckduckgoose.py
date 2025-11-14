030971ba5e4aeeb5441b1ced19f077317088f2c2
DomDodge <dominicdodge84@gmail.com>
2025-11-13 17:43:06 -0700

    aslkdjfa

--- duckduckgoose.py
+++ duckduckgoose.py
@@ -26,4 +26,6 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.run(debug=True, host='0.0.0.0', port=5005)
 
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005, debug=False, use_reloader=False,
+    threaded=True)
